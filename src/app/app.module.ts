7da88ef05c65df500fccfc7a16b61cb32b6fe9c8
Adrian Llerena <a.llerena001@gmail.com>
2018-08-04 16:03:44 -0400

    custom pipes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { BuiltInPipesComponent } from './built-in-pipes/built-in-pipes.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { BuiltInPipesComponent } from "./built-in-pipes/built-in-pipes.component";
+import { CustomPipesComponent } from "./custom-pipes/custom-pipes.component";
+import { CapitalizePipe } from "./pipes/capitalize.pipe";
+import { FilterPipe } from './pipes/filter.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    BuiltInPipesComponent
+    BuiltInPipesComponent,
+    CustomPipesComponent,
+    CapitalizePipe,
+    FilterPipe
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/built-in-pipes/built-in-pipes.component.ts
+++ src/app/built-in-pipes/built-in-pipes.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-built-in-pipes',
-  templateUrl: './built-in-pipes.component.html',
-  styleUrls: ['./built-in-pipes.component.css']
+  selector: "app-built-in-pipes",
+  templateUrl: "./built-in-pipes.component.html",
+  styleUrls: ["./built-in-pipes.component.css"]
 })
 export class BuiltInPipesComponent implements OnInit {
+  today = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  increment() {
+    const changedDate = new Date(this.today);
+
+    // increase the day of the month
+    changedDate.setDate(this.today.getDate() + 1);
+
+    // update our date
+    this.today = changedDate;
   }
-
 }
